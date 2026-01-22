f80b4a5bbe5d65c0a06eeb2041064f5eb8c55a8e
Anish-100 <anish10707@gmail.com>
2026-01-21 22:26:10 -0800

    Updated similiraties to tell if you have api key or not

--- similiraties.py
+++ similiraties.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
 
 if not api_key:
     print(f'The api key does not exist')
-
-print ('Hello World')
+else:
+    print(f'Api key recieved')
