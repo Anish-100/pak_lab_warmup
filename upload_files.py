53358cf59fa4651104388f95498363a78aa9725f
Anish-100 <anish10707@gmail.com>
2026-01-22 00:13:19 -0800

    Wrote code to upload files to Gemini, and save its data to a text file.  It saves the file_name, its gemini_name, then its uri.
    Issues: Find a better way to save to the text file.

--- upload_files.py
+++ upload_files.py
@@ -11,12 +11,26 @@ api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
     raise errors.APIERROR
     
-# client = genai.Client()
+client = genai.Client()
 
-for file in Path('./Imported Patient Notes').iterdir():
-    print(file)
-    # my_file = client.files.upload(file)
-    # with open('uri.txt', 'r') as uri_editor:
-    #     uri_editor.write(f'The File name:{file}')
-    #     uri_editor.write(f'It\'s URI: {my_file.uri}')
-    # print(f'URI {my_file.uri}')
+for file_name in Path('./Imported Patient Notes').iterdir():
+    print(file_name)
+    my_file = client.files.upload(file=file_name,
+    config = {
+        'display_name': f'{file_name}'
+    })
+    with open('./data/uri.txt', 'a') as uri_editor:
+        uri_editor.write(f'The File name:{file_name}')
+        uri_editor.write(f'It\'s Gemini Name: {my_file.name}')
+        uri_editor.write(f'It\'s URI: {my_file.uri}')
+        uri_editor.write(f'What is returned: {my_file}')
+    print(f'URI {my_file.uri}')
+
+with open('./data/uri.txt', 'r') as file:
+    content = file.read()
+    lines = content.split('The File name:')
+
+with open('./data/uri.txt', 'w') as f:
+    for i, part in enumerate(lines):
+        f.write(f"=== Part {i} ===\n") # Ignore Part 0
+        f.write(part.strip() + '\n\n')
